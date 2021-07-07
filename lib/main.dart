770ad4a3115f9407215a1f69630d494983a8dd2b
Yash Saxena <yash.saxena@gmail.com>
2021-07-06 20:09:46 -0500

    Removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Test Timer',
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
   }
